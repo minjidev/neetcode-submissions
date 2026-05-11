3afa78eaed10ce772f12436733bb04813b798255
minjidev <68722909+minjidev@users.noreply.github.com>
2026-05-11 09:02:29 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // 각 위치에서 이제까지의 최소값과 비교한다. 
+        for (let i=0;i<prices.length;i++) {
+            
+        }
+    }
+}
