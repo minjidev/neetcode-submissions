4e07090cb78b92066341f27d9e3af916587eeffd
minjidev <68722909+minjidev@users.noreply.github.com>
2026-05-11 09:11:27 +0900

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // 각 위치에서 이제까지의 최소값과 비교한다.
+
+        let minPrice = prices[0] 
+        let maxPrice = 0
+        for (let i=1;i<prices.length;i++) {
+            // 오늘 가격이 내렸으면 손해임. 팔지 않는다.
+            // 오늘 가격과 min 가격이 같은 경우는 이익이 0.
+            const todayPrice = prices[i]
+            if (todayPrice < minPrice) {
+                // 최소값 갱신
+                minPrice = todayPrice
+                continue
+            }
+
+            // 가격이 올랐다면? 
+            const todayProfit = todayPrice - minPrice
+            // 더 수익 많이 낼 수 있는 날이면 maxPrice 갱신 
+            if (maxPrice < todayProfit) {
+                maxPrice = todayProfit
+            }
+        }
+
+        return maxPrice
+    }
+}
